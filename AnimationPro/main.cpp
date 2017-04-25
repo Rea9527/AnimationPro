efc2e7d8b6b651eee95bdde02e3ab84781f08e21
陈主润 <32399723@qq.com>
2017-04-25 14:54:41 +0800

    create project

---
+++ AnimationPro/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AnimationPro
+//
+//  Created by 陈主润 on 18/04/2017.
+//  Copyright © 2017 陈主润. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
