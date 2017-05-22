4314479041b85bfb3e7b42385cb738e8e313df1e
陈主润 <32399723@qq.com>
2017-05-23 00:43:11 +0800

    delete animatedModel and collada parse and loades folders

--- AnimationPro/animation/loaders/AnimatedModelLoader.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  AnimatedModelLoader.cpp
-//  AnimationPro
-//
-//  Created by 陈主润 on 15/05/2017.
-//  Copyright © 2017 陈主润. All rights reserved.
-//
-
-#include "AnimatedModelLoader.hpp"
--- AnimationPro/animation/loaders/AnimationLoader.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  AnimationLoader.cpp
-//  AnimationPro
-//
-//  Created by 陈主润 on 15/05/2017.
-//  Copyright © 2017 陈主润. All rights reserved.
-//
-
-#include "AnimationLoader.hpp"
